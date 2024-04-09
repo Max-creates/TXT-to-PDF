cff6f1ec8ddcef91343196fd420e3b4c03cfc3ba
Max <maksym.solomyanov@gmail.com>
2024-04-09 17:46:55 +0200

    Add text to pdf pages.

--- main.py
+++ main.py
@@ -4,21 +4,29 @@ from pathlib import Path
 
 # Create a list of filepaths
 filepaths = glob.glob("files/*.txt")
-# Create a list of filenames without the extension and paths
-filenames = [Path(filepath).stem for filepath in filepaths]
 
 # Create one PDF file
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 # Go through the filenames
-for filename in filenames:
+for filepath in filepaths:
     # Add a page for each filename
     pdf.add_page()
+    
+    # Get the filename with the extension and convert it to title case
+    filename = Path(filepath).stem.title()
+    
     # Set the font
     pdf.set_font("Arial", "B", 16)
-    # Write the filename in the top-left corner and capitalize the first letter
-    # using the .title() method
-    pdf.cell(w=0, h=10, txt=filename.title(), ln=1, align="L")
+    
+    # Add the filename
+    pdf.cell(w=0, h=10, txt=filename, ln=1)
+    
+    # Read file and add text to pdf
+    with open(filepath, "r") as file:
+        text = file.read()
+        pdf.set_font("Arial", size=10)
+        pdf.multi_cell(w=0, h=6, txt=text)
 
 # Produce the PDF
 pdf.output("output.pdf")
